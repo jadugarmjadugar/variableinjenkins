f192e0f16f3f14d141d8c0f98c083bd2caecd67a
jadugarmjadugar <86535184+jadugarmjadugar@users.noreply.github.com>
2023-04-23 21:27:24 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-myVar="$1" # Access the variable value as the first argument
+myVar="$2" # Access the variable value as the first argument
 
 # Use myVar as needed in your shell script
 echo "My variable value is: ${myVar}"
